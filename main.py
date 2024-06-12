735841aabb2d9a0615b2b3e9ebbcd99254be0b36
ZX885 <zokirovzarruh@gmail.com>
2024-06-12 12:52:17 +0500

    12 06 last commit

--- main.py
+++ main.py
@@ -1,52 +1,105 @@
 import telebot
-import requests
-import webbrowser
-from telebot import types
+import sqlite3
+# import requests
+# import webbrowser
+# from telebot import types
 
 bot = telebot.TeleBot('7328354559:AAHF-zxxsIGWvMW4AUNbzDDnt2Oz6O-5wK4')
 print('Running the bot............')
-RANDOM_IMAGE = "/random_img"
-RANDOM_IMG_URL = "https://picsum.photos/1200"
-global IMAGE_COUNTER
-IMAGE_COUNTER = 0
+
+name = 'None'
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
-    # bot.send_photo(message.chat.id,photo='zx.jpg')
-    bot.send_message(message.chat.id,mess, parse_mode='html')
-@bot.message_handler(commands=['help'])
-def help(message):
-    mess = f"""
-    Добро пожаловать <b>{message.from_user.first_name} {message.from_user.last_name}</b>
-Полезные команды:
-    /start
-    /help
-    /site
-    /random_img
-        
-По всем вопросам @AKM_SHOOT
-    """
-    bot.send_message(message.chat.id, mess,parse_mode='html')
-
-@bot.message_handler(content_types=['photo'])
-def get_photo(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://www.youtube.com/channel/UCCEDRfofQS-DBurQ_MpBTkA'))
-    markup.add(types.InlineKeyboardButton('Удалить фото', callback_data='delete'))
-    markup.add(types.InlineKeyboardButton('Изменить текст', callback_data='edite'))
-    bot.reply_to(message, 'Какая красивое фото', reply_markup=markup)
+    conn = sqlite3.connect('zx.sql')
+    cur = conn.cursor()
     
-@bot.message_handler(commands=['site'])
-def site(message):
-    webbrowser.open('https://www.youtube.com/channel/UCCEDRfofQS-DBurQ_MpBTkA')
-@bot.message_handler(commands=['random_img'])
-def random_img(message):
-    if message.text == RANDOM_IMAGE:
-        image = get(RANDOM_IMG_URL).content
-        context.bot.sendMediaGroup(
-            chat_id=message.effective_chat.id,
-            media=[InputMediaPhoto(image, caption=f"Random {IMAGE_COUNTER}")]
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем! Введите имя')
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+def user_pass(message):
+    password = message.text.strip()
+    
+    conn = sqlite3.connect('zx.sql')
+    cur = conn.cursor()
+    
+    cur.execute(
+        f"INSERT INTO users (name, pass) VALUES ('%s', '%s')" % (name, password)
         )
+    conn.commit()
+    cur.close()
+    conn.close()
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Список пользователей', callback_data='users'))
+    bot.send_message(message.chat.id, 'Пользователь зарегистрирован!', reply_markup=markup)
+    # bot.register_next_step_handler(message, user_pass)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback(call):
+    conn = sqlite3.connect('zx.sql')
+    cur = conn.cursor()
+    
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+    
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}, пароль: {el[2]}\n'
+    cur.close()
+    conn.close()
+    
+    bot.send_message(call.message.chat.id, info)
+
+
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     markup = types.ReplyKeyboardMarkup()
+#     btn1 = types.KeyboardButton('Перейти на сайт')
+#     markup.row(btn1)
+#     btn2 = types.KeyboardButton('Удалить фото')
+#     btn3 = types.KeyboardButton('Изменить текст')
+#     markup.row(btn2, btn3)
+#     file = open('./zx.jpg', 'rb')
+#     bot.send_photo(message.chat.id, file)
+#     bot.send_message(message.chat.id, 'Привет 😂', reply_markup=markup)
+#     bot.register_next_step_handler(message, on_click)
+
+# ======================================================================================   
+# @bot.message_handler(commands=['help'])
+# def help(message):
+#     mess = f"""
+#     Добро пожаловать <b>{message.from_user.first_name} {message.from_user.last_name}</b>
+# Полезные команды:
+#     /start
+#     /help
+#     /site
+#     /random_img       
+# По всем вопросам @AKM_SHOOT
+#     """
+#     bot.send_message(message.chat.id, mess,parse_mode='html')
+# ===============================================================================
+# @bot.message_handler(content_types=['photo'])
+# def get_photo(message):
+#     markup = types.InlineKeyboardMarkup()
+#     btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://www.youtube.com/channel/UCCEDRfofQS-DBurQ_MpBTkA')
+#     markup.row(btn1)
+#     btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+#     btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edite')
+#     markup.row(btn2, btn3)
+#     bot.reply_to(message, 'Какая красивое фото', reply_markup=markup)
+
+
+
 
 bot.polling(non_stop=True)
