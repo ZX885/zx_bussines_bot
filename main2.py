d9386ba43c0ede27e57f82195fe2de9ec5514ae9
ZX885 <zokirovzarruh@gmail.com>
2024-06-22 23:09:04 +0500

    last

--- main2.py
+++ main2.py
@@ -1,4 +1,3 @@
-import socket
 import asyncio
 from aiogram import Bot, Dispatcher, types, F
 
@@ -20,19 +19,3 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Bot off')
-        
-        
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-
-print('connected:', addr)
-
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
-
-conn.close()
