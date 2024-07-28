43ddea45b816a7e34bc5ba8c384b4d200883c295
ZX885 <zokirovzarruh@gmail.com>
2024-07-28 23:01:51 +0500

    last

--- app/handlers.py
+++ app/handlers.py
@@ -11,16 +11,29 @@ router = Router()
 @router.message(CommandStart()) # должен ловить сообщение 
 async def start(message: Message):
     # await message.answer('Hello', reply_markup=kb.main)
-    await message.reply("How are you!")
+    await message.reply("""
+ Добро пожаловать в ZX Bussines bot
+Бот создан для личных целей и учебы.🔍
+
+/help 💁 для помощи
+/app 📟 наши сайты
+
+Если будут вобпросы тг на 👉 https://t.me/AKM_SHOOT
+
+""")
 
 @router.message(Command('help'))
 async def help(message: Message):
-    await message.answer('please wait......')
+    await message.answer('''
+/start 👆 для запуска бота 
+/app 📟 наши сайты
+    ''')
 
 @router.message(Command('app'))
 async def app(message: Message):
     markup = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text='web app',web_app=WebAppInfo(url='https://online-shop-rwi0.onrender.com'))],
-        [KeyboardButton(text='Channel',web_app=WebAppInfo(url='https://www.youtube.com/channel/UCCEDRfofQS-DBurQ_MpBTkA'))]
+        # [KeyboardButton(text='Channel',web_app=WebAppInfo(url='https://www.youtube.com/channel/UCCEDRfofQS-DBurQ_MpBTkA'))]
+        [KeyboardButton(text='Channel',web_app=WebAppInfo(url='https://www.youtube.com'))]
         ],resize_keyboard=True)
-    await message.answer('Hello!', reply_markup=markup)
+    await message.answer('Наши сайты 📟!', reply_markup=markup)
--- main2.py
+++ main2.py
@@ -8,7 +8,7 @@ print('Running a bot...........')
 
 
 async def main():
-    bot = Bot('7328354559:AAHF-zxxsIGWvMW4AUNbzDDnt2Oz6O-5wK4') # Подключаемся к боту через токен
+    bot = Bot('7328354559:AAHAxbG9MyD00Nx0ba7vf3crw6OT3_FsEUg') # Подключаемся к боту через токен
     dp = Dispatcher() # Наш обработчик, роутер, помошник
     dp.include_router(router)
     await dp.start_polling(bot) # Поллинг - наш скрипт обращается к серверу телеграм-- не пришло ли обновление
